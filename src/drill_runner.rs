ad8f50d3925c15a22afe9117e2bc3c08ef6fe528
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2022-11-29 16:51:14 +0100

    improve error reporting

--- src/drill_runner.rs
+++ src/drill_runner.rs
@@ -13,6 +13,7 @@ use qdrant_client::client::QdrantClient;
 use std::collections::HashMap;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
+use std::time::Duration;
 use tokio::sync::Semaphore;
 use tokio::time::Instant;
 
@@ -27,6 +28,12 @@ pub trait Drill: Send + Sync {
     async fn run(&self, client: &QdrantClient, args: Arc<Args>) -> Result<()>;
     // run before the first run
     async fn before_all(&self, client: &QdrantClient, args: Arc<Args>) -> Result<()>;
+}
+
+struct DrillReport {
+    uri: String,
+    duration: Duration,
+    error: Option<String>,
 }
 
 pub async fn run_drills(args: Args, stopped: Arc<AtomicBool>) -> Result<()> {
@@ -71,7 +78,7 @@ pub async fn run_drills(args: Args, stopped: Arc<AtomicBool>) -> Result<()> {
             let before_res = drill.before_all(&before_client_arc, args_arc.clone()).await;
             if let Err(e) = before_res {
                 error!(
-                    "ERROR: Drill {} failed to run before_all caused by {}",
+                    "Drill {} failed to run before_all caused by {}",
                     drill.name(),
                     e
                 );
@@ -90,11 +97,9 @@ pub async fn run_drills(args: Args, stopped: Arc<AtomicBool>) -> Result<()> {
             while !stopped.load(Ordering::Relaxed) {
                 // acquire semaphore to run
                 let run_permit = drill_semaphore.acquire().await.unwrap();
-                // at least one successful run necessary
-                let mut at_least_one_successful_run = false;
-
+                let mut drill_reports = vec![];
                 // run drill against all uri sequentially
-                info!("Running {}", drill.name());
+                info!("Starting {}", drill.name());
                 for uri in &args_arc.uris {
                     let drill_client = QdrantClient::new(Some(get_config(uri))).await.unwrap();
                     let execution_start = Instant::now();
@@ -102,40 +107,61 @@ pub async fn run_drills(args: Args, stopped: Arc<AtomicBool>) -> Result<()> {
                     match result {
                         Ok(_) => {
                             if let Some(_prev) = last_errors.remove(uri) {
-                                warn!("...{} is running again for {}", drill.name(), uri);
+                                warn!("{} is running again for {}", drill.name(), uri);
                             }
-                            info!(
-                                "...{} completed for {} in {:?}",
-                                drill.name(),
-                                uri,
-                                execution_start.elapsed()
+                            drill_reports.push(
+                                DrillReport {
+                                    uri: uri.to_string(),
+                                    duration: execution_start.elapsed(),
+                                    error: None,
+                                }
                             );
-                            at_least_one_successful_run = true;
                         }
                         Err(e) => {
+                            drill_reports.push(
+                                DrillReport {
+                                    uri: uri.to_string(),
+                                    duration: execution_start.elapsed(),
+                                    error: Some(e.to_string()),
+                                }
+                            );
                             // do not spam logs with the same error
                             if let Some(_prev_error) = last_errors.get(uri) {
                                 last_errors.insert(uri.to_string(), e);
                             } else {
-                                error!(
-                                    "ERROR: Drill {} failed for {} caused by {}",
-                                    drill.name(),
-                                    uri,
-                                    e
-                                );
+                                // print warning the first time
+                                warn!("{} failed for the first time for {} (see report)", drill.name(), uri);
                                 last_errors.insert(uri.to_string(), e);
                             }
                         }
                     }
                 }
 
-                if !at_least_one_successful_run {
-                    error!("ERROR: No successful runs for drill {}", drill.name());
+                // at least one successful run necessary
+                let successful_runs = drill_reports.iter().filter(|r| r.error.is_none()).count();
+
+                if successful_runs == 0 {
+                    error!("No successful runs for drill {}!", drill.name());
                     // stop coach in case pf no successful run if configured
                     if args_arc.stop_at_first_error {
                         stopped.store(true, Ordering::Relaxed);
                     }
+                } else if successful_runs == args_arc.uris.len() {
+                    info!(
+                        "{} finished successfully in {} seconds",
+                        drill.name(),
+                        drill_reports.iter().map(|r| r.duration.as_secs()).sum::<u64>()
+                    );
+                } else {
+                    let mut display_report = format!("{} failed partially: ", drill.name());
+                    for r in drill_reports {
+                        if let Some(e) = r.error {
+                            display_report.push_str(&format!("for {} ({}), ", r.uri, e));
+                        }
+                    }
+                    info!("{}", display_report);
                 }
+
                 // release semaphore while waiting for reschedule
                 drop(run_permit);
                 let reschedule_start = Instant::now();
