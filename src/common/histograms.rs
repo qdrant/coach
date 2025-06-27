46f6b1c4c6871cd0d8cd3df13fcd3d7e91629ad7
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2025-06-27 11:38:05 +0200

    fmt

--- src/common/histograms.rs
+++ src/common/histograms.rs
@@ -6,7 +6,5 @@ pub fn render_histogram(histogram: &Histogram<u64>) -> String {
     let p95 = histogram.value_at_quantile(0.95);
     let p99 = histogram.value_at_quantile(0.99);
     let max = histogram.max();
-    format!(
-        "[min: {min}ms, p50: {p50}ms, p95: {p95}ms, p99: {p99}ms, max: {max}ms]"
-    )
+    format!("[min: {min}ms, p50: {p50}ms, p95: {p95}ms, p99: {p99}ms, max: {max}ms]")
 }
